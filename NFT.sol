426e2d05b93faa1621cc5c4c447f6eb6648993a8
MarkFury <27889100+heiheiheiheiheihei@users.noreply.github.com>
2021-08-16 00:10:05 +0800

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -6,7 +6,7 @@ import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownabl
 /**
  * @dev This is an example contract implementation of NFToken with metadata extension.
  */
-contract ChineseGuoHua is
+contract NFT is
   NFTokenMetadata,
   Ownable
 {
